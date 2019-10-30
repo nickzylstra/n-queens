f50305edbd2055dc88cce28f2acc4c163155147d
nickzylstra <nick.zylstra@gmail.com>
2019-10-30 15:12:51 -0700

    Finishes row and col first tests

--- src/Board.js
+++ src/Board.js
@@ -62,7 +62,7 @@
     },
 
 
-/*
+    /*
          _             _     _
      ___| |_ __ _ _ __| |_  | |__   ___ _ __ ___ _
     / __| __/ _` | '__| __| | '_ \ / _ \ '__/ _ (_)
@@ -79,12 +79,31 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      const n = this.get('n');
+      const rowSqrs = this.get(rowIndex);
+      let occSqrCount = 0;
+      for (let i = 0; i < n; i += 1) {
+        if (rowSqrs[i]) {
+          occSqrCount += 1;
+          if (occSqrCount === 2) {
+            return true;
+          }
+        }
+      }
+
+      return false;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      const n = this.get('n');
+      for (var i = 0; i < n; i++) {
+        if (this.hasRowConflictAt(i)) {
+          return true;
+        }
+      }
+
+      return false;
     },
 
 
@@ -94,11 +113,29 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
+      const n = this.get('n');
+      let count = 0;
+      for (let i = 0; i < n; i++) {
+        const sqr = this.get(i)[colIndex];
+        if (sqr) {
+          count++;
+          if (count === 2) {
+            return true;
+          }
+        }
+      }
       return false; // fixme
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
+      const n = this.get('n');
+      for (var i = 0; i < n; i++) {
+        if (this.hasColConflictAt(i)) {
+          return true;
+        }
+      }
+
       return false; // fixme
     },
 
